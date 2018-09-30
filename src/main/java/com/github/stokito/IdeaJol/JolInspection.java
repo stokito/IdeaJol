7de3b89c90d4f5e96980a04a87e47e8be3063839
Sergey Ponomarev <stokito@gmail.com>
2018-10-01 01:47:24 +0300

    #9 Added an inspection which reports about too big classes

---
+++ src/main/java/com/github/stokito/IdeaJol/JolInspection.java
@@ -0,0 +1,80 @@
+package com.github.stokito.IdeaJol;
+
+import com.intellij.codeInspection.*;
+import com.intellij.psi.PsiClass;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.openjdk.jol.info.ClassData;
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.layouters.Layouter;
+
+public class JolInspection extends AbstractBaseJavaLocalInspectionTool {
+    private static final String[] BUSINESS_LOGIC_CLASS_MARKERS = {"Controller", "Service", "Strategy", "Adapter", "Exception", "Impl", "Dao"};
+    private static final Layouter LAYOUTER = Layouters.LAYOUTERS[5]; // hotspot coops
+    private static final long THRESHOLD = 64;
+
+    @Nullable
+    @Override
+    public ProblemDescriptor[] checkClass(@NotNull PsiClass aClass, @NotNull InspectionManager manager, boolean isOnTheFly) {
+        if (isNotUsualClass(aClass) || isBusinessLogicClass(aClass)) {
+            return null;
+        }
+        ClassData classData = PsiClassAdapter.createClassDataFromPsiClass(aClass);
+        ClassLayout layout = LAYOUTER.layout(classData);
+        if (layout.instanceSize() <= THRESHOLD) {
+            return null;
+        }
+        LocalQuickFix showJolAction = new ShowJolQuickFix();
+        ProblemDescriptor problem = manager.createProblemDescriptor(aClass, "Class have too big memory footprint", showJolAction, ProblemHighlightType.WEAK_WARNING, isOnTheFly);
+        return new ProblemDescriptor[]{problem};
+    }
+
+    /**
+     * @return true if class is anonymous, annotation, enum, interface, or is not exists in source
+     */
+    private boolean isNotUsualClass(PsiClass aClass) {
+        return aClass.getNameIdentifier() == null ||
+                aClass.isAnnotationType() ||
+                aClass.isEnum() ||
+                aClass.isInterface() ||
+                !aClass.isPhysical();
+    }
+
+    private boolean isBusinessLogicClass(PsiClass aClass) {
+        return endsWithAny(aClass.getName(), BUSINESS_LOGIC_CLASS_MARKERS);
+    }
+
+    // can be replaced with commons.lang3.StringUtils.endsWithAny
+    private boolean endsWithAny(String className, String[] businessLogicClassMarkers) {
+        if (className == null) return false;
+        for (String marker : businessLogicClassMarkers) {
+            if (className.endsWith(marker)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+/* looks like this approach works slower, needs to be better investigated with JMH
+    private static final Set<String> BUSINESS_LOGIC_CLASS_MARKERS_SET = new HashSet<>(Arrays.asList(BUSINESS_LOGIC_CLASS_MARKERS));
+
+    private int indexOfLastCamelCaseWord(String className) {
+        for (int i = className.length() - 1; i >= 0; i--) {
+            if (Character.isUpperCase(className.charAt(i))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean endsWithAnyWord(String className) {
+        int pos = indexOfLastCamelCaseWord(className);
+        if (pos == -1) {
+            return false;
+        }
+        String lastWordOfClassName = className.substring(pos);
+        return BUSINESS_LOGIC_CLASS_MARKERS_SET.contains(lastWordOfClassName);
+    }
+*/
+
+}
---
+++ src/main/java/com/github/stokito/IdeaJol/ShowJolQuickFix.java
@@ -0,0 +1,36 @@
+package com.github.stokito.IdeaJol;
+
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.ide.DataManager;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.ActionPlaces;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+
+public class ShowJolQuickFix implements LocalQuickFix {
+    @Nls(capitalization = Nls.Capitalization.Sentence)
+    @NotNull
+    @Override
+    public String getFamilyName() {
+        return "Show JOL";
+    }
+
+    @Override
+    public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor problemDescriptor) {
+        PsiElement psiClass = problemDescriptor.getPsiElement();
+        if (!(psiClass instanceof PsiClass)) {
+            return;
+        }
+        ActionManager am = ActionManager.getInstance();
+        AnActionEvent anActionEvent = new AnActionEvent(null, DataManager.getInstance().getDataContext(),
+                ActionPlaces.UNKNOWN, new Presentation(), am, 0);
+        am.getAction("showObjectLayout").actionPerformed(anActionEvent);
+
+    }
+}
