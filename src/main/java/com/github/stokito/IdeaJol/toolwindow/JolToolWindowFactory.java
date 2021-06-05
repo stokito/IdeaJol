4f0bf0c9ab373fca495bb0a64674454f265e6319
Sergey Ponomarev <stokito@gmail.com>
2021-06-05 17:57:10 +0300

    JolToolWindowFactory: avoid using JolView.getInstance()

--- src/main/java/com/github/stokito/IdeaJol/toolwindow/JolToolWindowFactory.java
+++ src/main/java/com/github/stokito/IdeaJol/toolwindow/JolToolWindowFactory.java
@@ -3,15 +3,17 @@ package com.github.stokito.IdeaJol.toolwindow;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.ui.content.ContentFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
 import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
 
 public class JolToolWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        JComponent jol = JolView.getInstance(project);
-        toolWindow.getContentManager().addContent(ContentFactory.SERVICE.getInstance().createContent(jol, null, false));
+        JolView jolView = new JolView(project);
+        ContentManager contentManager = toolWindow.getContentManager();
+        Content content = contentManager.getFactory().createContent(jolView, null, false);
+        contentManager.addContent(content);
+        content.setDisposer(jolView);
     }
 }
